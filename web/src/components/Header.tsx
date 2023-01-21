3558fab47ad90b45277b73da177aa1a7f1db801e
Wilson Franca <wilson.franca.92@gmail.com>
2023-01-21 07:30:33 -0800

    fix(web): added missing className flex to Header

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -3,7 +3,7 @@ import logoImage from '../assets/logo.svg'
 
 export function Header() {
   return (
-    <div className="w-full max-w-3xl mx-auto items-center justify-between">
+    <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
       <img src={logoImage} alt="Habits Logo" />
 
       <button 
