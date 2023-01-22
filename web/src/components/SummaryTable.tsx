93039493cba47772a280481c526ee88babe03d8d
Wilson Franca <wilson.franca.92@gmail.com>
2023-01-22 09:08:38 -0800

    feat(web): filled the grid with non-selectable squares until the minimum of days is filled

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -5,6 +5,12 @@ const WEEK_DAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 
 const summaryDates = generateDatesFromBeginningOfYear()
 
+const MIN_WEEKS = 18
+const DAYS_IN_A_WEEK = 7
+
+const minimumSummaryDateSize = MIN_WEEKS * DAYS_IN_A_WEEK
+const amountOfDaysToFill = minimumSummaryDateSize - summaryDates.length
+
 export function SummaryTable() {
   return (
     <div className="w-full flex gap-3">
@@ -22,6 +28,9 @@ export function SummaryTable() {
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map(date => {
           return <HabitDay key={date.toString()} />
+        })}
+        {amountOfDaysToFill > 0 && Array.from({length: amountOfDaysToFill}).map((_, index) => {
+          return <div key={index} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"/>
         })}
       </div>
     </div>
